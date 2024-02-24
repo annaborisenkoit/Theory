647dd7dc9aa6130df563dd6945d342ddca2deb6f
Anna Borisenko <anik4366@gmail.com>
2024-02-24 13:33:46 +0700

    Убираем локацию новой темы Primary из болванки

--- wp-content/themes/theory/functions.php
+++ wp-content/themes/theory/functions.php
@@ -70,12 +70,7 @@ function theory_setup() {
 		*/
 	add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'theory' ),
-		)
-	);
+
 
 	/*
 		* Switch default core markup for search form, comment form, and comments
