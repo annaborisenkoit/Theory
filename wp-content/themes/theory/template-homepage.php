6f10476ac89348c08f8523427b07ada08aa1526f
Anna Borisenko <anik4366@mail.ru>
2024-03-21 20:36:47 +0700

    12 Цикл на статической странице template-homepage.php

--- wp-content/themes/theory/template-homepage.php
+++ wp-content/themes/theory/template-homepage.php
@@ -3,7 +3,21 @@
  * Template name: Homepage Template
  */
 get_header();
+?>
 
-echo "custom content";
+<div>
 
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+    <?php get_template_part('partials/content'); ?> 
+
+<?php endwhile; else : ?>
+
+    <?php get_template_part('partials/content', 'none'); ?> 
+
+<?php endif; ?>
+
+</div>
+
+<?php
 get_footer();
