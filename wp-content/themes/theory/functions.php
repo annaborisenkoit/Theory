18ec96c96424ba2dd8bd4c3fbb2433d549dd075c
Anna Borisenko <anik4366@mail.ru>
2024-03-22 18:26:33 +0700

    хук для регистрации типа поста – он вызывается=выводится, когда вызывается хук init

--- wp-content/themes/theory/functions.php
+++ wp-content/themes/theory/functions.php
@@ -44,10 +44,51 @@ function theo_theme_init(){
 			)
 		);
 
-		load_theme_textdomain('theory', get_template_directory().'/lang');
+		load_theme_theory('theory', get_template_directory().'/lang');
 
 }
 add_action('after_setup_theme', 'theo_theme_init', 0);
+
+function theory_register_post_type(){
+	$args = array(
+		'label' => esc_html__('Cars', 'theory'),
+		'labels' => array(
+			'name'                  =>esc_html_x( 'Cars', 'Post type general name', 'theory' ),
+			'singular_name'         =>esc_html_x( 'Car', 'Post type singular name', 'theory' ),
+			'menu_name'             =>esc_html_x( 'Cars', 'Admin Menu text', 'theory' ),
+			'name_admin_bar'        =>esc_html_x( 'Car', 'Add New on Toolbar', 'theory' ),
+			'add_new'               =>esc_html__( 'Add New', 'theory' ),
+			'add_new_item'          =>esc_html__( 'Add New Car', 'theory' ),
+			'new_item'              =>esc_html__( 'New Car', 'theory' ),
+			'edit_item'             =>esc_html__( 'Edit Car', 'theory' ),
+			'view_item'             =>esc_html__( 'View Car', 'theory' ),
+			'all_items'             =>esc_html__( 'All Cars', 'theory' ),
+			'search_items'          =>esc_html__( 'Search Cars', 'theory' ),
+			'parent_item_colon'     =>esc_html__( 'Parent Cars:', 'theory' ),
+			'not_found'             =>esc_html__( 'No Cars found.', 'theory' ),
+			'not_found_in_trash'    =>esc_html__( 'No Cars found in Trash.', 'theory' ),
+			'featured_image'        =>esc_html_x( 'Car Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'theory' ),
+			'set_featured_image'    =>esc_html_x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'theory' ),
+			'remove_featured_image' =>esc_html_x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'theory' ),
+			'use_featured_image'    =>esc_html_x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'theory' ),
+			'archives'              =>esc_html_x( 'Car archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'theory' ),
+			'insert_into_item'      =>esc_html_x( 'Insert into Car', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'theory' ),
+			'uploaded_to_this_item' =>esc_html_x( 'Uploaded to this Car', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'theory' ),
+			'filter_items_list'     =>esc_html_x( 'Filter Cars list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'theory' ),
+			'items_list_navigation' =>esc_html_x( 'Cars list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'theory' ),
+			'items_list'            =>esc_html_x( 'Cars list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'theory' ),
+		),
+		'supports' => array('title', 'editor', 'author', 'thumbnail'),
+		'public' => true, 
+		'publicly_queryable' => true, 
+	);
+	register_post_type('car', $args);	
+}
+add_action('init', 'theory_register_post_type')
+
+
+
+
 
 
 
