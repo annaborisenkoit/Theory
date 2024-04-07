b3e8853e5de5ff328ec2ee6e5b2893fb2c797d5c
Anna Borisenko <anik4366@mail.ru>
2024-04-07 17:16:09 +0700

    20 если надо несколько разных query запросов - через разделитель на главной hr

--- wp-content/themes/theory/template-homepage.php
+++ wp-content/themes/theory/template-homepage.php
@@ -11,11 +11,35 @@ get_header();
         $args = array(
             
             'post_type' => 'car',            
-            'posts_per_page' => 1
+            'posts_per_page' => -1
         );
         $cars = new WP_Query($args); ?>
 
         <?php if($cars->have_posts()) : while($cars->have_posts()) : $cars->the_post(); ?>
+
+            <?php get_template_part('partials/content'); ?> 
+
+        <?php endwhile; else : ?>
+
+            <?php get_template_part('partials/content', 'none'); ?> 
+
+        <?php endif; 
+        
+        wp_reset_postdata();
+        ?>
+
+        <hr>
+
+        <?php
+        unset($args);
+        $args = array(
+            
+            'post_type' => 'post',            
+            'posts_per_page' => -1
+        );
+        $blogpost = new WP_Query($args); ?>
+
+        <?php if($blogpost->have_posts()) : while($blogpost->have_posts()) : $blogpost->the_post(); ?>
 
             <?php get_template_part('partials/content'); ?> 
 
