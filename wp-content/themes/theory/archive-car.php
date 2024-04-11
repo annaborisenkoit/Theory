df4094038ed0dfe1ed76f205b2d7d567c45b39b1
Anna Borisenko <anik4366@mail.ru>
2024-04-11 13:01:13 +0700

    21 персональная пагинация - по 1 машине на страницу

--- wp-content/themes/theory/archive-car.php
+++ wp-content/themes/theory/archive-car.php
@@ -21,11 +21,29 @@ get_header();
 				?>
 			</header><!-- .page-header -->
 
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<?php 
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$cars = new WP_Query(array('post_type' => 'car', 'posts_per_page' => 1, 'paged' => $paged));
+
+if($cars->have_posts()) : while($cars->have_posts()) : $cars->the_post(); ?>
 
 	<?php get_template_part('partials/content'); ?> 
 
-<?php endwhile; else : ?>
+<?php endwhile; ?>
+	<div class="pagination">
+		<?php
+			$big = 999999999; // need an unlikely integer
+
+			echo paginate_links( array(
+				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+				'format' => '?paged=%#%',
+				'current' => max( 1, get_query_var('paged') ),
+				'total' => $cars->max_num_pages
+			) );
+		?>
+	</div>
+<?php
+else : ?>
 
 	<?php get_template_part('partials/content', 'none'); ?> 
 
