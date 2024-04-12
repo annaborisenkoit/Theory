7cb883d83f9d6b246834ca253653d2f4e751b2c0
Anna Borisenko <anik4366@mail.ru>
2024-04-12 11:35:16 +0700

    22 - своя пагинация - функция theory_paginate($cars)

--- wp-content/themes/theory/archive-car.php
+++ wp-content/themes/theory/archive-car.php
@@ -31,16 +31,7 @@ if($cars->have_posts()) : while($cars->have_posts()) : $cars->the_post(); ?>
 
 <?php endwhile; ?>
 	<div class="pagination">
-		<?php
-			$big = 999999999; // need an unlikely integer
-
-			echo paginate_links( array(
-				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-				'format' => '?paged=%#%',
-				'current' => max( 1, get_query_var('paged') ),
-				'total' => $cars->max_num_pages
-			) );
-		?>
+		<?php theory_paginate($cars) ?>
 	</div>
 <?php
 else : ?>
--- wp-content/themes/theory/functions.php
+++ wp-content/themes/theory/functions.php
@@ -9,6 +9,17 @@
 
  require get_template_directory() . '/inc/widget-about.php';
 
+ function theory_paginate($query){
+	$big = 999999999; // need an unlikely integer
+
+	echo paginate_links( array(
+		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+		'format' => '?paged=%#%',
+		'current' => max( 1, get_query_var('paged') ),
+		'total' => $query->max_num_pages
+	) );
+ }
+
  function theory_widgets_init() {
 	register_sidebar(
 		array(
