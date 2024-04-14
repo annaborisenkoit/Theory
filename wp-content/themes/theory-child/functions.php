0bbe2c06a6209c68cbcb284ef8a27aa364f3f0ea
Anna Borisenko <anik4366@mail.ru>
2024-04-14 13:53:13 +0700

    23 новый functions.php theory_child_widgets_init

---
+++ wp-content/themes/theory-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function theory_child_widgets_init() {
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'New Pages Sidebar', 'theory' ),
+			'id'            => 'newsidebar',
+			'description'   => esc_html__( 'Appear as Sidebar on New Pages', 'theory' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+}
+add_action( 'widgets_init', 'theory_widgets_init' );
