de3035f1efd03acaeecb4d3af47de1bd148f3aff
Anna Borisenko <anik4366@mail.ru>
2024-05-11 13:48:13 +0700

    31 добавила виджет about-widget.php

--- wp-content/plugins/theory-core/theory-core.php
+++ wp-content/plugins/theory-core/theory-core.php
@@ -18,8 +18,10 @@ if ( !function_exists( 'add_action' ) ) {
 	exit;
 }
 
+
 require plugin_dir_path( __FILE__ ) . '/inc/widget-about.php';
 require plugin_dir_path( __FILE__ ) . '/inc/metaboxes.php';
 require plugin_dir_path( __FILE__ ) . '/inc/acf.php';
 require plugin_dir_path( __FILE__ ) . '/inc/custom_post_type.php';
+require plugin_dir_path( __FILE__ ) . '/inc/elementor.php';
 
