40f03564fb8cf1df53c2f74f6b62a1754b60d49c
Anna Borisenko <anik4366@mail.ru>
2024-06-21 15:57:12 +0700

    40 в template fullwidth.php убрала get_template_part и установила в теге main весь контент - для того, чтобы убрать заголовок, который прописала в админке

--- wp-content/themes/theory/template-fullwidth.php
+++ wp-content/themes/theory/template-fullwidth.php
@@ -7,7 +7,7 @@ get_header();
     while ( have_posts() ) :
         the_post();
 
-        get_template_part('partials/content', 'page'); 
+        echo '<main>'.the_content().'</main>';
 
         // If comments are open or we have at least one comment, load up the comment template.
         if ( comments_open() || get_comments_number() ) :
