10be0a5568d4f2b64ed7760f1866d7ce96cc9d48
Anna Borisenko <anik4366@mail.ru>
2024-06-30 14:29:07 +0700

    41 вывод фото в виджет команды

--- wp-content/plugins/theory-core/inc/widgets/js/theo-team.js
+++ wp-content/plugins/theory-core/inc/widgets/js/theo-team.js
@@ -2,43 +2,38 @@
 
     $(window).on("elementor/frontend/init", function(){
 
-        console.log("elementor frontend init fired");
-
         elementorFrontend.hooks.addAction("frontend/element_ready/theo_team.default", function($scope, $){
-            console.log("Element ready: theo_team.default");
-            alert('test');
+            
+        $(".team-carousel, .related-carousel").owlCarousel({
+            autoplay: true,
+            smartSpeed: 1000,
+            center: true,
+            margin: 30,
+            dots: false,
+            loop: true,
+            nav : true,
+            navText : [
+                '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+                '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+            ],
+            responsive: {
+                0:{
+                    items:1
+                },
+                576:{
+                    items:1
+                },
+                768:{
+                    items:2
+                },
+                992:{
+                    items:3
+                }
+            }
+        });
         });
     });
 
 })(jQuery);
 
 
-/*
-$(".team-carousel, .related-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    center: true,
-    margin: 30,
-    dots: false,
-    loop: true,
-    nav : true,
-    navText : [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0:{
-            items:1
-        },
-        576:{
-            items:1
-        },
-        768:{
-            items:2
-        },
-        992:{
-            items:3
-        }
-    }
-});
-*/
